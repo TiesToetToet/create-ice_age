d06131653baee27f1d5ac4fabfb99ee418c84c04
TiesToetToet <77145356+TiesToetToet@users.noreply.github.com>
2023-11-16 18:19:00 +0100

    added bounding boxes

--- src/main/java/com/miketies/create_ice_age/super_freezer/lid/BasinFreezerLidBlock.java
+++ src/main/java/com/miketies/create_ice_age/super_freezer/lid/BasinFreezerLidBlock.java
@@ -3,8 +3,14 @@ package com.miketies.create_ice_age.super_freezer.lid;
 import com.miketies.create_ice_age.block.IABlockEntities;
 import com.miketies.create_ice_age.item.IAItems;
 import com.simibubi.create.foundation.block.IBE;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BasinFreezerLidBlock extends Block implements IBE<BasinFreezerLidBlockEntity> {
     public BasinFreezerLidBlock(Properties pProperties) {
@@ -15,6 +21,17 @@ public class BasinFreezerLidBlock extends Block implements IBE<BasinFreezerLidBl
     public Class<BasinFreezerLidBlockEntity> getBlockEntityClass() {
         return BasinFreezerLidBlockEntity.class;
     }
+
+    @Override
+    public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return Shapes.empty();
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return Shapes.or(box(0, 0, 0, 16, 2, 16), box(6, 2, 6, 10, 3, 10));
+    }
+
 
     @Override
     public BlockEntityType<? extends BasinFreezerLidBlockEntity> getBlockEntityType() {
